dbf1d1efe30875056a95efbd58b9132cf9baae63
Moderator11 <p7925221@gmail.com>
2025-09-28 22:29:07 +0900

    refactor: video_to_summarization function separation from main

--- svsp-ai/main.py
+++ svsp-ai/main.py
@@ -1,6 +1,4 @@
-from utils.video_to_audio import convert_video_to_audio
-from utils.audio_to_text import transcribe_audio
-from utils.llm_client import call_gemini
+from utils.video_to_summarization import video_to_summarization
 import logging, shutil, os
 
 logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s',
@@ -12,21 +10,7 @@ logging.debug("Logging started.")
 def main():
     # VIDEO_PATH = "./examples/Kurzgesagt - Alcohol is AMAZING.mp4"
     VIDEO_PATH = "{target video path}"
-    CACHE_PATH = "./cache"
-
-    audio_file = convert_video_to_audio(VIDEO_PATH, CACHE_PATH)
-
-    audio_path = os.path.join(CACHE_PATH, audio_file)
-    text = transcribe_audio(audio_path)
-    logging.debug(text)
-
-    prompt = f"This is sentence with corresponding timestamp. {str(text)} Please pick important sentence and remove the rest. Try to make total length maximum up to 1 minute. Right your answer in a same format"
-    res = call_gemini("gemini-2.5-flash", prompt)
-    print(res['text'])
-
-    if os.path.exists(CACHE_PATH):
-        shutil.rmtree(CACHE_PATH)
-        logging.debug(f"Removed cache folder: {CACHE_PATH}")
+    video_to_summarization(VIDEO_PATH)
 
 
 if __name__ == '__main__':
