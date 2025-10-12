3e6cfcfb79f8ad1628c6a144aed9ab93f2753c86
Moderator11 <p7925221@gmail.com>
2025-10-12 18:58:27 +0900

    refact: Router code separation from App.tsx

--- svsp-fe/src/App.tsx
+++ svsp-fe/src/App.tsx
@@ -1,29 +1,7 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import MyPage from "./pages/MyPage";
+import Router from "./components/Router";
 
-const App: React.FC = () => {
-  return (
-    <Router>
-      <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
-        <h1>React 회원 시스템</h1>
-
-        <nav style={{ marginBottom: "20px" }}>
-          <Link to="/" style={{ marginRight: "10px" }}>로그인</Link>
-          <Link to="/register" style={{ marginRight: "10px" }}>회원가입</Link>
-          <Link to="/mypage">마이페이지</Link>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/mypage" element={<MyPage />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-};
+function App() {
+  return <Router />;
+}
 
 export default App;
