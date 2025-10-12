6877a274624b56a6673a26a1570a2a559902830d
Moderator11 <p7925221@gmail.com>
2025-10-12 19:18:45 +0900

    fix: Apply alias path

--- svsp-fe/src/App.tsx
+++ svsp-fe/src/App.tsx
@@ -1,6 +1,6 @@
+import NavigationBar from "@components/NavigationBar";
+import Router from "@router/Router";
 import { BrowserRouter } from "react-router-dom";
-import NavigationBar from "./components/NavigationBar";
-import Router from "./components/Router";
 
 function App() {
   return (
