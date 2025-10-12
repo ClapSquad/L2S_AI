f59aa79a55e97df5393eea65110454c135eec479
Moderator11 <p7925221@gmail.com>
2025-10-12 19:13:34 +0900

    project: Alias setup, Add MyPage

--- svsp-fe/src/components/NavigationBar.tsx
+++ svsp-fe/src/components/NavigationBar.tsx
@@ -4,12 +4,15 @@ export default function NavigationBar() {
   return (
     <nav style={{ marginBottom: "20px" }}>
       <Link to="/" style={{ marginRight: "10px" }}>
+        홈페이지
+      </Link>
+      <Link to="/login" style={{ marginRight: "10px" }}>
         로그인
       </Link>
       <Link to="/register" style={{ marginRight: "10px" }}>
         회원가입
       </Link>
-      <Link to="/mypage">마이페이지</Link>
+      <Link to="/my">마이페이지</Link>
     </nav>
   );
 }
--- svsp-fe/src/components/Router.tsx
+++ svsp-fe/src/components/Router.tsx
@@ -1,14 +1,16 @@
 import { Route, Routes } from "react-router-dom";
-import LoginPage from "../pages/login/LoginPage";
-import RegisterPage from "../pages/register/RegisterPage";
-import MyPage from "../pages/my/MyPage";
+import MainPage from "@main/MainPage";
+import LoginPage from "@login/LoginPage";
+import RegisterPage from "@register/RegisterPage";
+import MyPage from "@my/MyPage";
 
 export default function Router() {
   return (
     <Routes>
-      <Route path="/" element={<LoginPage />} />
+      <Route path="/" element={<MainPage />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-      <Route path="/mypage" element={<MyPage />} />
+      <Route path="/my" element={<MyPage />} />
     </Routes>
   );
 }
