75bb08bdd6f81ce30421a009abc4704d45bf5a08
Moderator11 <p7925221@gmail.com>
2025-10-25 01:41:19 +0900

    feat: add auth functionality

---
+++ svsp-be/app/api/auth/me.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, Request, HTTPException, status
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["Auth"])
+
+
+@router.get("/me")
+async def get_current_user(request: Request):
+    print(request.session)
+    user = request.session.get("user")
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Require login"
+        )
+    return {"user": user}
