059ebf71165a60f8b9a9bab7fdcf664b73e92684
cinmon <gianellaalessandrac@gmail.com>
2025-11-09 22:28:34 +0900

    chores: fix debugging prints

--- svsp-ai/utils/highlight_detection/highlight_pipeline.py
+++ svsp-ai/utils/highlight_detection/highlight_pipeline.py
@@ -9,12 +9,23 @@ def run_echofusion(video_path, title="", summary="", llm_timestamps=[], **overri
     cfg = yaml.safe_load(open(pathlib.Path(__file__).with_name("config.yaml")))
     cfg["fusion"].update({k:v for k,v in overrides.items() if k in cfg["fusion"]}) # Override fusion params as given
 
-    print("\n--- Step 1: Shot Detection & Keyframe Extraction ---")
-    shots = detect_shots(video_path, init_threshold=cfg["segmentation"]["scenedetect_threshold"])
-    print("-> Extracting Keyframes ...")
-    extract_keyframes(video_path, shots, fps=cfg["segmentation"]["keyframe_fps"])
+    # Shot Detection
+    print("\n--- Step 1: Shot Detection ---")
+    if not pathlib.Path(f"data/shots/{video_path.stem}.shots.json").exists():
+        shots = detect_shots(video_path, init_threshold=cfg["segmentation"]["scenedetect_threshold"])
+    else:
+        print("-> Shots detected, loading from file.")
+        shots = json.load(open(f"data/shots/{video_path.stem}.shots.json"))
 
-    print("\n--- Step 2: Compute Branch Scores ---")
+    # Keyframe Extraction
+    print("\n--- Step 2: Keyframe Extraction ---")
+    if not pathlib.Path("data/keyframes") / video_path.stem / "shot_0000" .exists():
+        print("-> Extracting keyframes ...")    
+        extract_keyframes(video_path, shots, fps=cfg["segmentation"]["keyframe_fps"])
+    else:
+        print("-> Keyframes already extracted, skipping.")
+
+    print("\n--- Step 3: Compute Branch Scores ---")
     print("-> HD Branch")
     hd = compute_hd_branch(video_path, shots, title, summary, cfg["hd_branch"])
     stem = pathlib.Path(video_path).stem
@@ -32,8 +43,8 @@ def run_echofusion(video_path, title="", summary="", llm_timestamps=[], **overri
     txt_path = f"data/shots/{stem}.txt.json"
     json.dump(txt, open(txt_path,"w"), indent=2)
     print(f"-> Done. TXT scores saved to {txt_path}")
-    
-    print("\n--- Step 3: Fusion and Highlight Selection ---")
+
+    print("\n--- Step 4: Fusion and Highlight Selection ---")
     predictions = fuse_and_select(video_path, hd_path, txt_path,
         w_hd=cfg["fusion"]["w_hd"],
         w_txt=cfg["fusion"]["w_txt"],
